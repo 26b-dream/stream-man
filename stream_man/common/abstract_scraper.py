da85f6b554adcf5fd22adba428876ce82b3071ee
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-06 22:34:33 -0700

    Removed uneeded abstract function

--- stream_man/common/abstract_scraper.py
+++ stream_man/common/abstract_scraper.py
@@ -8,10 +8,6 @@ from typing import Optional
 class AbstractScraperClass(ABC):
     """All scraper must inherit and implement this class to be detected"""
 
-    @abstractmethod
-    def __init__(self, url: str) -> None:
-        """Regular initialization method"""
-
     @classmethod
     @abstractmethod
     def is_valid_show_url(cls, show_url: str) -> bool:
@@ -21,4 +17,4 @@ class AbstractScraperClass(ABC):
     def update(
         self, minimum_info_timestamp: Optional[datetime] = None, minimum_modified_timestamp: Optional[datetime] = None
     ) -> None:
-        """Update the information for a show"""
+        """Update the information for the show"""
