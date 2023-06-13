c6a557b101e31e2a6e82d4688743181443b84683
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-13 15:21:46 -0700

    Made path more robust

--- stream_man/common/scrapers.py
+++ stream_man/common/scrapers.py
@@ -1,17 +1,18 @@
 """"Scraper class that will return the correct concrete scraper for a URL
 
-To do this scrapers must be imported, initializing a Scraper object will import all scrapers in the scrapers folder"""
+To do this scrapers must be imported, initializing a Scraper object will import all valid scrapers in the scrapers
+folder`"""
 import importlib
 
 from common.abstract_scraper import AbstractScraperClass
-from extended_path import ExtendedPath
+from constants import BASE_DIR
 
 
 def import_scrapers():
     """Import all plugins in the scrapers folder"""
 
     # Directory of all scrapers
-    scrapers_dir = ExtendedPath(__file__).parent.parent / "scrapers"
+    scrapers_dir = BASE_DIR / "scrapers"
 
     # Import each scraper
     for scraper in scrapers_dir.glob("*"):
@@ -32,7 +33,7 @@ class Scraper:
 
     import_scrapers()
 
-    def __new__(cls, url: str):
+    def __new__(cls, url: str) -> AbstractScraperClass:
         for subclass in AbstractScraperClass.__subclasses__():
             if subclass.is_valid_show_url(url):
                 return subclass(url)
