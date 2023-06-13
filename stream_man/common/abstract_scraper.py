a4c65ec520f521514a73755d8e22efb67a270b4a
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-13 15:00:38 -0700

    Updated comments

--- stream_man/common/abstract_scraper.py
+++ stream_man/common/abstract_scraper.py
@@ -1,31 +1,41 @@
-"""Abstract class that all scrapers must inherit and implement"""
-
+"""Includes AbstractScraperClass, which all scraper plugins must inherit and implement"""
 from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Optional
 
 
 class AbstractScraperClass(ABC):
-    """All scraper must inherit and implement this class to be detected"""
+    """Abstract class that must be inherited and implemented by plugins for them to be loaded"""
 
-    @property
     @abstractmethod
-    def WEBSITE(self):
-        ...
+    def website_name(self):
+        """Name of the website that the scraper is for"""
 
     @classmethod
     @abstractmethod
     def is_valid_show_url(cls, show_url: str) -> bool:
-        """Check if a URL is a valid show URL for a specific scraper"""
+        """Check if a url is a valid url for the scraper
+
+        Args:
+            show_url (str): The url to check
+
+        Returns:
+            bool: True if the url is valid, False otherwise
+        """
 
     @abstractmethod
     def update(
         self, minimum_info_timestamp: Optional[datetime] = None, minimum_modified_timestamp: Optional[datetime] = None
     ) -> None:
-        """Update the information for the show"""
+        """Update the information for the show
+
+        Args:
+            minimum_info_timestamp (Optional[datetime], optional): A datetime that the information should be newer than.
+            Defaults to None.
+            minimum_modified_timestamp (Optional[datetime], optional): A datetime that the information in the database
+            should be newer than. Defaults to None.
+        """
 
     @abstractmethod
-    def show_object(
-        self, minimum_info_timestamp: Optional[datetime] = None, minimum_modified_timestamp: Optional[datetime] = None
-    ) -> None:
+    def show_object(self) -> None:
         """The Show object from the database"""
