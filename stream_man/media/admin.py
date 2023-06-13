8ec3c6e84572fa19cede2d4a3de5b4bd477f870c
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-13 11:47:56 -0700

    Register all media models in admin

--- stream_man/media/admin.py
+++ stream_man/media/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Episode, EpisodeWatch, Season, Show, UpdateQue
+
+admin.site.register(Show)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(EpisodeWatch)
+admin.site.register(UpdateQue)
