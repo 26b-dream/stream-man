54d66dd33989f44be7ed14cfe10b66a9ed99ae3a
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-13 12:02:17 -0700

    Added playlist models to admin

--- stream_man/playlists/admin.py
+++ stream_man/playlists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Playlist, PlaylistImportQueue, PlaylistShow
+
+admin.site.register(Playlist)
+admin.site.register(PlaylistShow)
+admin.site.register(PlaylistImportQueue)
