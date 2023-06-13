db38baed6e5d21225c7bdcf13856a4e75241ca39
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-13 15:23:34 -0700

    Updated comments

--- stream_man/media/apps.py
+++ stream_man/media/apps.py
@@ -1,6 +1,9 @@
+"""Django app config for the media app."""
 from django.apps import AppConfig
 
 
 class MediaConfig(AppConfig):
+    """Django app config for the media app."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "media"
