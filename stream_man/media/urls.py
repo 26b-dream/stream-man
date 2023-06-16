b943acf62b7f3fed14ba56e6f50862d269567951
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-16 15:20:06 -0700

    Fixed incorrect view name

--- stream_man/media/urls.py
+++ stream_man/media/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("show/<int:show_id>/", views.Indexes.show, name="show"),
     path("season/<int:season_id>/", views.Indexes.season, name="season"),
     path("episode/<int:episode_id>/", views.Indexes.episode, name="episode"),
-    path("mark_episode_watched_form/", views.mark_episode_watched_form, name="mark_episode_watched_form"),
+    path("mark_episode_watched_form/", views.Forms.mark_episode_watched_form, name="mark_episode_watched_form"),
 ]
