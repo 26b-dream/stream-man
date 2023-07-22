ef2fd4d048c815a4cfd00bf3af89a1ab0de2a7f1
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-07-22 14:23:49 -0700

    Improved doc string

--- stream_man/common/abstract_scraper.py
+++ stream_man/common/abstract_scraper.py
@@ -1,8 +1,9 @@
 """Contains AbstractScraperClass, which all scraper plugins must inherit and implement"""
 # @abstractmethod Seems to break if TYPE_CHECKING, so just do a full import of everything
 from abc import ABC, abstractmethod
-from typing import Optional
 from datetime import datetime
+from typing import Optional
+
 from media.models import Show
 
 
@@ -19,27 +20,13 @@ class AbstractScraperClass(ABC):
     @classmethod
     @abstractmethod
     def is_valid_show_url(cls, show_url: str) -> bool:
-        """Check if a url is a valid url for the scraper
-
-        Args:
-            show_url (str): The url to check
-
-        Returns:
-            bool: True if the url is valid, False otherwise
-        """
+        """Check if a url is a valid url for the scraper"""
 
     @abstractmethod
     def update(
         self, minimum_info_timestamp: Optional[datetime] = None, minimum_modified_timestamp: Optional[datetime] = None
     ) -> None:
-        """Update the information for the show
-
-        Args:
-            minimum_info_timestamp (Optional[datetime], optional): A datetime that the information should be newer than.
-            Defaults to None.
-            minimum_modified_timestamp (Optional[datetime], optional): A datetime that the information in the database
-            should be newer than. Defaults to None.
-        """
+        """Update the information for the show"""
 
     @abstractmethod
     def show_object(self) -> Show:
