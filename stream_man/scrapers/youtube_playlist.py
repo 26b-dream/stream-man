73d049ee142fe7db65c7969e6fc4a92a4720d9f4
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-11-19 01:12:12 -0800

    Small variable change

--- stream_man/scrapers/youtube_playlist.py
+++ stream_man/scrapers/youtube_playlist.py
@@ -223,7 +223,7 @@ class YouTubePlaylist(ScraperShowShared, AbstractScraperClass):
                     episode.number = str(i)
                     episode.description = episode_json_parsed["description"]
                     episode.duration = episode_json_parsed["duration"]
-                    episode.url = f"https://youtu.be/{partial_episode['id']}"
+                    episode.url = f"https://youtu.be/{episode.id}"
 
                     date = datetime.strptime(episode_json_parsed["upload_date"], "%Y%m%d").astimezone()
                     episode.air_date = date
