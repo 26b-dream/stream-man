e1682392ddcb6a8e007d90605ae2a92973347256
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2024-03-18 19:47:37 -0700

    Re lint

--- stream_man/media/forms.py
+++ stream_man/media/forms.py
@@ -1,18 +1,23 @@
 """Django forms for the media app."""
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from django import forms
 
 from .models import EpisodeWatch
 
+if TYPE_CHECKING:
+    from typing import ClassVar
+
 
 class MarkEpisodeWatchedForm(forms.ModelForm):
-    """Form for marking an episode as watched"""
+    """Form for marking an episode as watched."""
 
-    class Meta:  # pyright: ignore [reportIncompatibleVariableOverride]
+    class Meta:  # type: ignore  # noqa: PGH003, D106 - Meta has false positives
         model = EpisodeWatch
-        fields = ["episode", "watch_date"]
-        widgets = {
+        fields = ("episode", "watch_date")
+        widgets: ClassVar = {
             "watch_date": forms.DateInput(format=("%d-%m-%Y"), attrs={"type": "date"}),
             "episode": forms.HiddenInput(),
         }
