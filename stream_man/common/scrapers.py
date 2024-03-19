88a7851e6c1ab54973e2590959313d66c3e99e2f
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2024-03-18 19:40:21 -0700

    Re lint

--- stream_man/common/scrapers.py
+++ stream_man/common/scrapers.py
@@ -1,21 +1,25 @@
-""""Scraper class that will return the correct concrete scraper for a URL
+""""Scraper class that will return the correct concrete scraper for a URL.
 
 To do this scrapers must be imported, initializing a Scraper object will import all valid scrapers in the scrapers
-folder`"""
+folder.
+"""
+from __future__ import annotations
+
 import importlib
 
 from common.abstract_scraper import AbstractScraperClass
 from common.constants import BASE_DIR
 
 
-def import_scrapers():
-    """Import all plugins in the scrapers folder"""
-
+def import_scrapers() -> None:
+    """Import all plugins in the scrapers folder."""
     # Directory of all scrapers
     scrapers_dir = BASE_DIR / "scrapers"
 
     # Import each scraper
     for scraper in scrapers_dir.glob("*"):
+        # Not used in any official scrapers, but allow third party scrapers to be in a subfolder if the author wants to
+        # write it that way
         if scraper.is_dir():
             scraper_path = f"scrapers.{scraper.name}"
             importlib.import_module(scraper_path)
@@ -25,34 +29,45 @@ def import_scrapers():
 
 
 class InvalidURLError(Exception):
-    """Exception raised when a URL is invalid"""
+    """Exception raised when a URL is invalid."""
 
 
 class Scraper:
-    """Scraper class that will return the correct concrete scraper for a URL"""
+    """Scraper class that will return the correct concrete scraper for a URL."""
 
     import_scrapers()
 
     def __new__(cls, url: str) -> AbstractScraperClass:
-        for subclass in AbstractScraperClass.__subclasses__():
-            # pyright note - This only returns concrete subclasses, so I do not need to worry about that the function is
-            # not implemented
-            if subclass.is_valid_show_url(url):  # pyright: ignore[reportGeneralTypeIssues]
-                return subclass(url)  # pyright: ignore[reportGeneralTypeIssues]
+        """Return the correct concrete scraper for a URL.
 
-        raise InvalidURLError(f"Invalid url {url}")
+        Parameters:
+        ----------
+        url (str): The URL to get the scraper for
+
+        Returns:
+        -------
+        AbstractScraperClass: The concrete scraper for the URL which has to be a subclass of AbstractScraperClass
+        """
+        for subclass in AbstractScraperClass.__subclasses__():
+            if subclass.is_valid_show_url(url):
+                return subclass(url)
+        error_message = f"No scraper found for {url}"
+        raise InvalidURLError(error_message)
 
     @classmethod
     def credential_keys(cls) -> list[tuple[str, list[str]]]:
+        """Return a list of tuples with the website name and the credential keys for each scraper.
+
+        Returns:
+        -------
+        list[tuple[str, list[str]]]: A list of tuples with the website name and the credential keys for each scraper.
+        """
         # This would technically be faster if it used list comprehension, but it makes it much harder to read so the
-        # speed penalty is acceptable
+        # speed penalty is acceptable for a small thing like credential keys
         output: list[tuple[str, list[str]]] = []
         for subclass in AbstractScraperClass.__subclasses__():
             if subclass.credential_keys():
-                # pyright note - This only returns concrete subclasses, so I do not need to worry about that the
-                # function is not implemented. In addition this is a seperate variable because having the comment on the
-                # same line makes the comment too long and it gets wrapped and breaks
-                website_name = subclass.website_name()  # pyright: ignore[reportGeneralTypeIssues]
+                website_name = subclass.website_name()
 
                 output.append((website_name, subclass.credential_keys()))
 
