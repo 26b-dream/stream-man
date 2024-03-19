1e2cc5fc08fd86597491534f38e59fd263cc35d9
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2024-03-18 19:09:08 -0700

    Re-linted

--- stream_man/common/extended_re.py
+++ stream_man/common/extended_re.py
@@ -1,7 +1,9 @@
+"""Extended regular expression module."""
 from __future__ import annotations
 
 # Import all of re so it can be exported, one of the few places were a wildcard import is acceptable
-from re import *  # pylint: disable=W0622,W0401,W0614 # noqa: F403
+# Have to make a stupid line of code for all the linters I use that hate wildcard imports
+from re import *  # type: ignore  # pylint: disable=W0622,W0401,W0614 # noqa: F403, PGH003
 from re import search
 from typing import TYPE_CHECKING
 
@@ -9,17 +11,29 @@ if TYPE_CHECKING:
     from re import Match, Pattern
 
 
-class StrictPatternFailure(Exception):
-    """Raised when a strict pattern fails to match"""
+class StrictPatternError(Exception):
+    """Raised when a strict pattern fails to match."""
 
 
 def strict_search(pattern: Pattern[str] | str, string: str) -> Match[str]:
-    """Scan through string looking for a match to the pattern, returning a Match object, or raise StrictPatternFailure
-    if no match was found.
+    """Stricter version of re.search.
+
+    If a match is found it returns the match object, otherwise it raises a StrictPatternError.
+
+    Parameters:
+    ----------
+    pattern (Pattern[str] | str): The pattern to search for
+    string (str): The string to search in
+
+    Returns:
+    -------
+    Match[str]: The match object
 
     Raises:
-        StrictPatternFailure: If no match is found"""
+        StrictPatternError: If no match is found
+    """
     output = search(pattern, string)
     if output is None:
-        raise StrictPatternFailure(f"{string} did not include {pattern}")
+        error_message = f"Pattern {pattern} not found in {string}"
+        raise StrictPatternError(error_message)
     return output
