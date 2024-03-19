3f8780ab30239816de54882374507b746ce60d97
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2024-03-18 18:47:33 -0700

    Updated to use PavedPath

--- stream_man/common/constants.py
+++ stream_man/common/constants.py
@@ -1,12 +1,12 @@
-"""Constant values that are frequently reused"""
+"""Constants used throughout the project."""
 
 from __future__ import annotations
 
-from extended_path import ExtendedPath
+from paved_path import PavedPath
 
 from stream_man.settings import BASE_DIR as _BASE_DIR
 
-# Update the value to use ExtendedPath instead of a regular path
-BASE_DIR = ExtendedPath(_BASE_DIR)
+# Update the value to use PavedPath instead of a regular path
+BASE_DIR = PavedPath(_BASE_DIR)
 
 DOWNLOADED_FILES_DIR = BASE_DIR / "downloaded_files"
